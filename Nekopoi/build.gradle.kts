fab4538dbfa6c75e4a80ee0c3f6a35328a9526c5
Sahrul431 <65979362+Sahrul431@users.noreply.github.com>
2024-04-13 22:19:32 +0700

    Update build.gradle.kts

--- Nekopoi/build.gradle.kts
+++ Nekopoi/build.gradle.kts
@@ -17,7 +17,31 @@ cloudstream {
      * 3: Beta only
      * */
     status = 1 // will be 3 if unspecified
+    tvTypes = listOf(      // use an integer for version numbers
+   version = 4
+
+
+   cloudstream {
+    language = "en"
+    // All of these properties are optional, you can safely remove them
+
+    // description = "Lorem Ipsum"
+     authors = listOf("Sora")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1 // will be 3 if unspecified
     tvTypes = listOf(
+        "NSFW",
+    )
+
+    iconUrl = "https://www.google.com/s2/favicons?domain=missav.com&sz=%size%"
+}
         "NSFW",
     )
 
